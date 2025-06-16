e327f0ad922174dd1c27aaaefa97c2562baf8740
Nešad Omeragić <nesad.omeragich27@gmail.com>
2025-06-16 23:04:52 +0200

    Add functionality for creating circles

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,83 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
+import { Stage, Layer, Text, Circle, Group } from 'react-konva';
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [circles, setCircles] = useState<{ x: number, y: number, radius: number }[]>([]);
+  const [startPos, setStartPos] = useState<{ x: number, y: number } | null>(null);
+
+  const handleOnMouseDown = (e) => {
+    setStartPos({
+      x: e.evt.offsetX,
+      y: e.evt.offsetY
+    })
+
+    console.log("UP -> ", e);
+
+
+    // if (e.target.index !== 0)
+    //   return;
+
+    // setCircles(prev => [
+    //   ...prev,
+    //   {
+    //     x: xPos,
+    //     y: yPos
+    //   }
+    // ])
+  }
+
+  const handleOnMouseUp = (e) => {
+
+    if (startPos === null)
+      return;
+
+    const endPosX = e.evt.offsetX;
+    const endPosY = e.evt.offsetY;
+
+    const newCircle = { x: startPos.x, y: startPos.y, radius: endPosX - startPos.x };
+
+    setCircles(prev => [
+      ...prev,
+      newCircle
+    ])
+  }
+
+  const handleClearOnClick = () => {
+    setCircles([]);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='wrapper'>
+      <Stage width={800} height={600} fontSize={15} onMouseDown={handleOnMouseDown} onMouseUp={handleOnMouseUp}>
+        <Layer >
+          <Text text='Test 123' />
+          {
+            circles.map((c, i) => (
+              <Group
+                key={i}>
+                <Circle
+                  x={c.x}
+                  y={c.y}
+                  radius={c.radius + 2}
+                  fill={"black"}
+                />
+                <Circle
+                  x={c.x}
+                  y={c.y}
+                  onClick={(e) => console.log(e)}
+                  radius={c.radius}
+                  fill="green">
+
+                </Circle>
+              </Group>
+            ))
+          }
+        </Layer>
+      </Stage>
+      <button type='button' onClick={handleClearOnClick}>Clear</button>
+    </div>
   )
 }
 
